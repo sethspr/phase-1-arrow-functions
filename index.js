0485f35baeb2799869ddd5832b6628fb78299a8f
seth spring <seth.c.spring@gmail.com>
2024-02-03 11:28:43 -0500

    arrow functions lab complete

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (a, b) => a / b;
+    console.log(divide(2000, 20));
+
+const square = a => a * a;
+    console.log(square(5, 10));
+
+const add = (a, b) => a + b;
+    console.log(add(5, 10));
